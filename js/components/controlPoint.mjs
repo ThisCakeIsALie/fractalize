15ce0bd56765fa3bd5a3b468a3d22494afc051ec
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2020-05-18 16:54:03 +0200

    Fix alignment issues of control points and simplify

--- js/components/controlPoint.mjs
+++ js/components/controlPoint.mjs
@@ -8,6 +8,7 @@ export const ControlPoint = {
         <style>
             :host {
                 display: inline-block;
+                position: relative;
                 width: ${host.size}px;
                 height: ${host.size}px;
             }
@@ -16,67 +17,18 @@ export const ControlPoint = {
                 display: none;
             }
 
-            #wrapper {
-                display: flex;
-                alignItems: center;
-                justifyContent: center;
-                position: relative;
+            #container {
                 width: 100%;
-                height: 100%;
+                heigth: 100%;
             }
-
-            .circle {
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                top: 0;
-                margin: auto;
-            }
-
-            #innermostCircle {
-                width: 25%;
-                height: 25%;
-                border-radius: 50%;
-                background: black;
-            }
-
-            #innerCircle {
-                position: absolute;
-                width: 75%;
-                height: 75%;
-                background: rgba(0, 0, 0, 0.2);
-                border-radius: 50%;
-                background: white;
-            }
-
-            #outerCircle {
-                position: absolute;
-                width: 85%;
-                height: 85%;
-                background: rgba(0, 0, 0, 0.2);
-                border-radius: 50%;
-                background: black;
-            }
-
-            #outermostCircle {
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                background: rgba(0, 0, 0, 0.2);
-                border-radius: 50%;
-                background: white;
-            }
-
-
 
         </style>
-        <div id="wrapper">
-            <div class="circle" id="outermostCircle"></div>
-            <div class="circle" id="outerCircle"></div>
-            <div class="circle" id="innerCircle"></div>
-            <div class="circle" id="innermostCircle"></div>
-        </div>
+        <svg id="container" viewBox="0 0 100 100">
+            <circle cx=50 cy=50 r=50 fill=white></circle>
+            <circle cx=50 cy=50 r=40 fill=black></circle>
+            <circle cx=50 cy=50 r=35 fill=white></circle>
+            <circle cx=50 cy=50 r=12 fill=black></circle>
+        </svg>
     `
 };
 
